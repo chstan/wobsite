165c5daa909747ae9e1e44fd129302e77261fd34
Conrad Stansbury <chstansbury@gmail.com>
2015-03-04 17:58:16 -0800

    Fixed string encoding bug.

--- Views/Scheme.hs
+++ Views/Scheme.hs
@@ -2,8 +2,10 @@ module Views.Scheme
        (schemeJSONView) where
 
 import Data.List (intercalate)
+import Data.Aeson
+import qualified Data.ByteString.Lazy.Char8 as BSL8
 
 schemeJSONView :: [String] -> String
 schemeJSONView [] = "{}"
 schemeJSONView ls = "{\"out\":[" ++
-  (intercalate "," (fmap (\x -> ("\"" ++ x ++ "\"")) ls)) ++ "]}"
+  (intercalate "," (fmap (BSL8.unpack . encode . toJSON) ls)) ++ "]}"
